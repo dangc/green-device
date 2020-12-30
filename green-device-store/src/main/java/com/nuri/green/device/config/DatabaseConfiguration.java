598400b4962b373f78317e1affb6fdb47bd643df
윤경희 <kh.yoon@nuritelecom.com>
2020-12-30 14:17:47 +0900

    NDGN-48 Initial commit

---
+++ green-device-store/src/main/java/com/nuri/green/device/config/DatabaseConfiguration.java
@@ -0,0 +1,32 @@
+package com.nuri.green.device.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:application.yml")
+public class DatabaseConfiguration {
+	
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource.hikari.master")
+	public HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DataSource dataSource = new HikariDataSource(hikariConfig());
+		return dataSource;
+	}
+
+}
